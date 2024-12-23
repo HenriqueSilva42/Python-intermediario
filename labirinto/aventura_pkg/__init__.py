54ac23e55a4d9fb8c716c7d3e94110b4a50a93fd
HenriqueSilva42 <167092193+HenriqueSilva42@users.noreply.github.com>
2024-12-23 18:52:37 -0300

    Update __init__.py

--- labirinto/aventura_pkg/__init__.py
+++ labirinto/aventura_pkg/__init__.py
@@ -1,10 +1,3 @@
-# Inicializa o pacote aventura_pkg
-
-# Importa as funções e classes dos módulos dentro do pacote
 from .labirinto import criar_labirinto, imprimir_labirinto
-from .jogador import Jogador, mover, escutar_comandos
+from .jogador import Jogador, escutar_comandos
 from .utils import imprime_instrucoes, imprime_menu
-
-# Agora você pode acessar as funções e classes diretamente a partir do pacote aventura_pkg
-# Exemplo:
-# from aventura_pkg import criar_labirinto
