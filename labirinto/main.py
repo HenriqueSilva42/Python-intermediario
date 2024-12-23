03fe40fbff52cbc5c019e8746c4dbc09f9bde02f
HenriqueSilva42 <167092193+HenriqueSilva42@users.noreply.github.com>
2024-12-23 18:39:39 -0300

    Update main.py

--- labirinto/main.py
+++ labirinto/main.py
@@ -1,1 +1,32 @@
+import argparse
+from aventura_pkg.labirinto import criar_labirinto, imprimir_labirinto
+from aventura_pkg.jogador import Jogador
+from aventura_pkg.utils import imprime_instrucoes, imprime_menu
+
+def main():
+    parser = argparse.ArgumentParser(description="Jogo Aventura no Labirinto")
+    parser.add_argument("--name", required=True, help="Nome do jogador")
+    parser.add_argument("--dificuldade", type=int, default=5, help="Dificuldade do labirinto (tamanho)")
+    parser.add_argument("--disable-sound", action="store_true", help="Desativa o som")
+    args = parser.parse_args()
+
+    imprime_menu()
+
+    escolha = input("Escolha uma opção: ")
+
+    if escolha == '1':  # Jogar
+        labirinto = criar_labirinto(args.dificuldade)
+        imprimir_labirinto(labirinto)
+        jogador = Jogador(args.name, labirinto)
+        jogador.escutar_comandos()
+
+    elif escolha == '2':  # Instruções
+        imprime_instrucoes()
+
+    elif escolha == '3':  # Sair
+        print("Saindo do jogo...")
+        return
+
+if __name__ == "__main__":
+    main()
 
